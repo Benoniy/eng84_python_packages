3e574107d591f39e56aef5f8b2a217b46e5d2ce4
Ben <benranson5@gmail.com>
2021-03-25 14:27:32 +0000

    did packages.py

---
+++ README.md
@@ -0,0 +1,6 @@
+# Python Packages
+### What is a Package?
+### Why should we use packages?
+### What is PIP?
+* PIP is a commonly used package manager that is used to install additional python packages
+* `pip install package_name` - This is the command used to install a package
---
+++ packages.py
@@ -0,0 +1,23 @@
+import random  # adds random number generation
+import math
+
+num_float = 23.6
+
+print(math.ceil(num_float))
+
+# Modules
+import os
+import datetime
+import sys
+import platform
+
+
+working_dir = os.getcwd()
+print(working_dir)
+
+print(os.cpu_count())
+
+print(datetime.datetime.now())
+
+print(sys.path)
+
